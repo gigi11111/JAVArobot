7123a22429ac90c6219c789729d1ed1f4c3d3250
reded <redede1@gmail.com>
2022-04-15 15:48:35 +0200

    derniers attributs du diagramme UML. 15/04 Géraud

--- Game.java
+++ Game.java
@@ -1,6 +1,11 @@
+import java.util.List;
+
 public class Game {
     static int nbrtour;
     static int deplacement;
+    List<Player> players;
+    List<Jeton> jetons;
+    List<Map> map;
     public static void game(){
 
     }
